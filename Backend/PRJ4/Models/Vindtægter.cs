b926b87c45779e58bf360de6188f52832e9aa2b0
SenpaiTP <thomascool01@gmail.com>
2024-10-30 13:12:18 +0100

    good start to controllers, keys, and dtos, and repo

--- Backend/PRJ4/Models/Vindtægter.cs
+++ Backend/PRJ4/Models/Vindtægter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PRJ4.Models;
 
 public partial class Vindtægter
 {
+    [Key]
     public int VindtægtId { get; set; }
 
     public int BrugerId { get; set; }

81fcc7160dba71a5800e23416fe21486b691e3d6
Thea Rønneholt <202210051@post.au.dk>
2024-10-30 10:25:56 +0100

    Git ignore .csproj

--- Backend/PRJ4/obj/Debug/net8.0/PRJ4.AssemblyInfo.cs
+++ Backend/PRJ4/obj/Debug/net8.0/PRJ4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRJ4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15260fdfd85e77bdfaa580da4f54a492b52b4256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84882e14f1c7728e285913ef770420edbebbe07a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRJ4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRJ4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

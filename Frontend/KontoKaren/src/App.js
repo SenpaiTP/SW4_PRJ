c618eb08e907be0265d12b8c6ffd3bd3525f2502
Kasper <202209866@post.au.dk>
2024-10-30 12:24:07 +0100

    Welcome to homepage

--- Frontend/KontoKaren/src/App.js
+++ Frontend/KontoKaren/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import HomePage from './Pages/Homepage';
 import { CssBaseline } from '@mui/material';
+import HomePage from './Pages/Homepage';
 
 function App() {
   return (
-    <Router>
+    <>
       <CssBaseline />
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<Navigate to="/home" />} />
-      </Routes>
-    </Router>
+      <HomePage />
+    </>
   );
 }
 

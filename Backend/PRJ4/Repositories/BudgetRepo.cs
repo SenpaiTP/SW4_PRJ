6ab358028b68879581b5b44a877ce6bd68af795e
Thea Rønneholt <202210051@post.au.dk>
2024-10-30 13:14:41 +0100

    Budget Repository - First iteration

---
+++ Backend/PRJ4/Repositories/BudgetRepo.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using PRJ4.Models;
+using PRJ4.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRJ4.Repositories;
+
+public class BudgetRepository: TemplateRepo<Budget>, IBudget
+{
+    private readonly ApplicationDbContext _context;
+    public BudgetRepository(ApplicationDbContext context) : base(context) 
+    {
+        _context = context;
+    }
+
+    public async Task<Budget> GetBudgetbyBudgetId(int budgetId)
+    {
+        return await _context.Set<Budget>()
+            .FirstOrDefaultAsync(budget => budget.BudgetId == budgetId);
+    }
+
+}
---
+++ Backend/PRJ4/Repositories/IBudgetRepo.cs
@@ -0,0 +1,8 @@
+
+using PRJ4.Models;
+namespace PRJ4.Repositories;
+
+public interface IBudget:ITemplateRepo<Budget>
+{
+
+}

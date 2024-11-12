810652321f745c1bc2ae59e18b92382553640826
Thea Rønneholt <202210051@post.au.dk>
2024-11-12 20:49:46 +0100

    Create BudgetRepo.cs
    Created budget repository

---
+++ Backend/PRJ4/Repositories/BudgetRepo.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using PRJ4.Models;
+using PRJ4.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRJ4.Repositories;
+
+public class BudgetRepository: TemplateRepo<Budget>, IBudget
+{
+    private readonly ApplicationDbContext _context;
+    public BudgetRepository(ApplicationDbContext context) : base(context) 
+    {
+        _context = context;
+    }
+
+}

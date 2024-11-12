a550bca5bffa53822af90ad4e160ad4bbd412f6f
Thea Rønneholt <202210051@post.au.dk>
2024-11-12 21:28:40 +0100

    Update BudgetDTO.cs
    Updated DTO with attributes

---
+++ Backend/PRJ4/DTO/BudgetDTO.cs
@@ -0,0 +1,10 @@
+using PRJ4.Models;
+
+namespace PRJ4.DTOs;
+public class BudgetDTO
+{
+    public int? BrugerId { get; set; }
+    public int SavingsGoal { get; set; }
+    public int MonthlySavingsAmount { get; set; }
+    public int SavingsPeriodInMonths { get; set; }
+}

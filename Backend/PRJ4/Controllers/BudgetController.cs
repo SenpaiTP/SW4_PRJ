9d66cbac6fdb331f4a36434b0fb8042543b07102
Thea Rønneholt <202210051@post.au.dk>
2024-11-12 21:29:43 +0100

    Update BudgetController.cs
    Updated with GET and POST methods

--- Backend/PRJ4/Controllers/BudgetController.cs
+++ Backend/PRJ4/Controllers/BudgetController.cs
@@ -1,5 +1,6 @@
 using PRJ4.Repositories;
 using PRJ4.Models;
+using PRJ4.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -18,7 +19,7 @@ public class BudgetController : ControllerBase
 
         // GET
         [HttpGet]
-        public async Task<IActionResult> GetAll(int id)
+        public async Task<IActionResult> GetAllBudgets() //Get all budgets
         {
             var budget = await _budgetRepository.GetAllAsync();
 
@@ -28,4 +29,42 @@ public class BudgetController : ControllerBase
             }
             return Ok(budget);
         }
+
+        // GET: api/budget/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBudget(int id)
+        {
+            var budget = await _budgetRepository.GetByIdAsync(id); 
+            if (budget == null)
+            {
+                return BadRequest($"No budgets with id {id}."); 
+            }
+
+            return Ok(budget); 
+        }
+
+
+        // POST
+        [HttpPost] 
+        public IActionResult AddBudget(BudgetDTO budgetDTO) //Add a budget
+        {
+            if (budgetDTO == null) 
+            {
+                return BadRequest("Budget values cannot be null");
+            }
+            
+            var budget = new Budget
+            {
+                BrugerId = budgetDTO.BrugerId,
+                SavingsGoal = budgetDTO.SavingsGoal,
+                MonthlySavingsAmount = budgetDTO.MonthlySavingsAmount,
+                SavingsPeriodInMonths = budgetDTO.SavingsPeriodInMonths
+            };
+
+            var createdBudget = _budgetRepository.AddAsync(budget);
+
+            return CreatedAtAction(nameof(GetBudget), new { id = createdBudget.Id }, createdBudget);
+
+        }
+            
     }
